9105b4aeb8f3c53471d28482a7238b5370242c1d
t-izuno <t-izuno@nri-net.com>
2020-08-12 10:59:14 +0900

    init

---
+++ src/test/kotlin/io/nncdevel/notificationmailcollector/ApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.nncdevel.alertmailcollector
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AlertMailCollectorApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
